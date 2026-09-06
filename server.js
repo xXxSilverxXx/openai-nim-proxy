ac69fee2725c2baa1328783019fbce0e1d68da51
xXxSilverxXx <75926387+xXxSilverxXx@users.noreply.github.com>
2026-09-06 04:16:12 -0500

    Replace Nvidia model with Minimax AI model

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ var MODEL_CACHE_MS = 10 * 60 * 1000;
 // ============================================================
 
 var FALLBACK_MODELS = [
-  "nvidia/nemotron-3-ultra-550b-a55b"
+  "minimaxai/minimax-m3"
 ];
 
 
@@ -79,7 +79,7 @@ var FALLBACK_MODELS = [
 // ============================================================
 
 var NON_THINKING_MODELS = {
-  "nvidia/nemotron-3-ultra-550b-a55b": false
+  "minimaxai/minimax-m3": false
 };
 
 
